dc7a346fb6b8fde0c6d63778ca7e9f00d9199135
Austin Kincaid & Lindsay Culver <student@epicodus.com>
2015-08-11 11:11:04 -0700

    Add spec and method to empty all places

--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -22,4 +22,12 @@ describe(Place) do
       expect(Place.all()).to(eq([test_place]))
     end
   end
+
+  describe('.clear') do
+    it('empties all places saved') do
+      Place.new('kentucky').save()
+      Place.clear()
+      expect(Place.all()).to(eq([]))
+    end
+  end
 end
